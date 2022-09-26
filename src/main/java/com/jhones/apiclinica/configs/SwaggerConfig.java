24092bcc441353d9eb5cb7854bb99d59c4cf3aab
Jhones Henrique <jhoneshenriquedasilva@gmail.com>
2022-09-26 11:35:23 -0300

    Adding Swagger to the project

---
+++ src/main/java/com/jhones/apiclinica/configs/SwaggerConfig.java
@@ -0,0 +1,57 @@
+package com.jhones.apiclinica.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.jhones.apiclinica"))
+                .paths(regex("/patient.*")) //URI root
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    //API Information
+    private ApiInfo metaInfo() {
+
+        ApiInfo apiInfo = new ApiInfo(
+                "Clínica Média Restful API",
+                "Essa é API de uma clínica médica",
+                "1.0",
+                "Terms of Service",
+                new Contact("Jhones Henrique", "https://github.com/jhoneshenrique/api-clinica",
+                        "jhoneshenriquedasilva@gmail.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>()
+        );
+        return apiInfo;
+    }
+
+    @Bean
+    public LinkDiscoverers discoverers() {
+        List<LinkDiscoverer> plugins = new ArrayList<>();
+        plugins.add(new CollectionJsonLinkDiscoverer());
+        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+    }
+}
--- src/main/java/com/jhones/apiclinica/resources/PatientResource.java
+++ src/main/java/com/jhones/apiclinica/resources/PatientResource.java
@@ -2,11 +2,14 @@ package com.jhones.apiclinica.resources;
 
 import com.jhones.apiclinica.models.Patient;
 import com.jhones.apiclinica.services.PatientService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@Api(value = "Clínica médica API Restful - Pacientes")
 @RestController
 @RequestMapping("/patient")
 public class PatientResource {
@@ -15,6 +18,7 @@ public class PatientResource {
     PatientService patientService;
 
     //List all the patients
+    @ApiOperation(value = "Retorna uma lista de pacientes")
     @GetMapping(produces = "application/json")
     public @ResponseBody Iterable<Patient> listAllPatients(){
         Iterable<Patient> patients = patientService.loadAllPatients();
@@ -22,6 +26,7 @@ public class PatientResource {
     }
 
     //List a specific patient by its id
+    @ApiOperation(value = "Retorna um paciente específico pelo código")
     @GetMapping(value ="/{id}" ,produces = "application/json")
     public @ResponseBody Patient listPatient(@PathVariable("id") long id){
         Patient patient = patientService.findPatientById(id);
@@ -29,18 +34,21 @@ public class PatientResource {
     }
 
     //Save a patient
+    @ApiOperation(value = "Cadastra um paciente")
     @PostMapping
     public Patient savePatient(@RequestBody @Valid Patient patient){
         return patientService.save(patient);
     }
 
     //Update the data from the patient
+    @ApiOperation(value = "Atualiza os dados de um paciente")
     @PutMapping
     public Patient updatePatient(@RequestBody @Valid Patient patient){
         return patientService.update(patient);
     }
 
     //Delete a patient
+    @ApiOperation(value = "Delete um paciente")
     @DeleteMapping
     public Patient deletePatient(@RequestBody Patient patient){
         patientService.delete(patient);
